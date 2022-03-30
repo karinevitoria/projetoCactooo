3e73885c920be56757701419836f28882ce4b779
karinevitoria <karinevfalves@gmail.com>
2022-03-30 14:58:06 -0300

    Adicionando a tela jogo

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+
+const NUMBER_OF_GUESSES = 8;
+let guessesRemaining = NUMBER_OF_GUESSES;
+let currentGuess = [];
+let nextLetter = 0;
+let rightGuessString = "";
+console.log(rightGuessString)
+
+function initBoard() {
+    let board = document.getElementById("game-board");
+
+    for (let i = 0; i < NUMBER_OF_GUESSES; i++) {
+        let row = document.createElement("div")
+        row.className = "letter-row"
+        
+        for (let j = 0; j < 5; j++) {
+            let box = document.createElement("div")
+            box.className = "letter-box"
+            row.appendChild(box)
+        }
+
+        board.appendChild(row)
+    }
+}
+
+initBoard()
